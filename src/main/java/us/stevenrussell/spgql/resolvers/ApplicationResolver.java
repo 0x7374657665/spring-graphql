d002f1206b3c2bbe2681f7d0d7f31dabd108b320
Steve Russell <0x7374657665@gmail.com>
2018-12-08 18:07:43 -0500

    removed some cruft

--- src/main/java/us/stevenrussell/spgql/resolvers/ApplicationResolver.java
+++ src/main/java/us/stevenrussell/spgql/resolvers/ApplicationResolver.java
@@ -4,11 +4,7 @@ import com.coxautodev.graphql.tools.GraphQLResolver;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.servlet.GraphQLContext;
 import org.dataloader.DataLoader;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import us.stevenrussell.spgql.repositories.RoleRepository;
 import us.stevenrussell.spgql.types.Application;
 import us.stevenrussell.spgql.types.Entitlement;
 import us.stevenrussell.spgql.types.Role;
@@ -18,15 +14,6 @@ import java.util.concurrent.CompletableFuture;
 
 @Component
 public class ApplicationResolver implements GraphQLResolver<Application> {
-
-    @Autowired
-    private JdbcTemplate jdbc;
-
-    @Autowired
-    private RoleRepository roleRepo;
-
-    private static final BeanPropertyRowMapper ROLE_MAPPER = new BeanPropertyRowMapper(Role.class);
-    private static final BeanPropertyRowMapper ENTITLEMENT_MAPPER = new BeanPropertyRowMapper(Entitlement.class);
 
     public CompletableFuture<Role> getProvisionerRole(Application application, DataFetchingEnvironment dfe) {
         DataLoader<Long, Role> provisionersForApplicationsDataLoader = ((GraphQLContext) dfe.getContext())
